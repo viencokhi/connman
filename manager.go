0e5e9d982a853bdd1c3ac7c6fceb6e1afe3ec6f9
emre <ocakemrecan@gmail.com>
2020-08-15 22:09:32 +0300

    new status and remove key functions added, connectedTo function migrated here

--- manager.go
+++ manager.go
@@ -1,10 +1,79 @@
 package connman
 
 import (
+	"fmt"
 	"jin"
 	"penman"
+	"strings"
 	"time"
 )
+
+var (
+	configPath string = "/etc/wifi.conf"
+)
+
+//Status network and connection status
+func Status() []byte {
+	status := []byte(`{"internet":null, "connections":null, "connectedTo":null, "availableNetworks":null, "savedNetworks":null,"error":null}`)
+	connections := jin.MakeJsonWithMap(getConnections())
+	available, err := GetNetworks()
+	if err != nil {
+		status, _ = jin.AddKeyValueString(status, "error", err.Error())
+		return status
+	}
+	saved := ReadNetworks()
+	availableNetworks := jin.MakeArrayString(available)
+	status, err = jin.SetBool(status, ConnectedToInternet(), "internet")
+	if err != nil {
+		status, _ = jin.SetString(status, err.Error(), "error")
+		return status
+	}
+	status, err = jin.Set(status, connections, "connections")
+	if err != nil {
+		status, _ = jin.SetString(status, err.Error(), "error")
+		return status
+	}
+	connected := ConnectedTo()
+	if connected != "" {
+		status, err = jin.SetString(status, connected, "connectedTo")
+	}
+	if err != nil {
+		status, _ = jin.SetString(status, err.Error(), "error")
+		fmt.Println("here", status)
+		return status
+	}
+	status, err = jin.Set(status, availableNetworks, "availableNetworks")
+	if err != nil {
+		status, _ = jin.SetString(status, err.Error(), "error")
+		return status
+	}
+	status, err = jin.Set(status, saved, "savedNetworks")
+	if err != nil {
+		status, _ = jin.SetString(status, err.Error(), "error")
+		return status
+	}
+	status, err = jin.SetString(status, "null", "error")
+	if err != nil {
+		status, _ = jin.SetString(status, err.Error(), "error")
+		return status
+	}
+	return status
+}
+
+//ConnectedTo returns SSID of wifi connection
+func ConnectedTo() string {
+	err := interfaceUp()
+	if err != nil {
+		return ""
+	}
+	cmd := fmt.Sprintf(`nmcli connection show | grep "%v"`, wifiInterface)
+	out, _ := exe(cmd, "connected to")
+	index := strings.Index(out, " ")
+	if index == -1 {
+		return ""
+	}
+	return out[:index]
+}
 
 //ConnectAvailable scan, find and connect a saved available wifi network
 func ConnectAvailable() error {
@@ -63,6 +132,21 @@ func SaveNetwork(networkName, passphrase string) error {
 		configs = jin.MakeEmptyJson()
 	}
 	configs, err = jin.AddKeyValueString(configs, networkName, passphrase)
+	if err != nil {
+		return err
+	}
+	penman.OWrite(configPath, configs)
+	return nil
+}
+
+//RemoveNetwork remove network from config path
+func RemoveNetwork(network string) error {
+	if result, _ := hasThisNetwork(network); !result {
+		return nil
+	}
+	var err error
+	configs := ReadNetworks()
+	configs, err = jin.Delete(configs, network)
 	if err != nil {
 		return err
 	}
