8d703071f55ed6616d7cb66453a6da6304f96d2e
emre <ocakemrecan@gmail.com>
2020-08-15 23:49:11 +0300

    automation functions added to drive startup service

---
+++ automation.go
@@ -0,0 +1,36 @@
+package connman
+
+import (
+	"fmt"
+	"time"
+)
+
+var (
+	hotspotTime   time.Duration = 5 * time.Minute
+	nodeResetTime time.Duration = 1 * time.Minute
+	mainSpot      *Spot         = NewSpot("solarnode", "")
+)
+
+func startUp() {
+	lastStatus = status()
+	hotspotOn(mainSpot)
+	go startToListen()
+	time.Sleep(hotspotTime)
+	hotspotOff(mainSpot)
+	server.Close()
+	connectAvailable()
+}
+
+func nodeLoop() {
+	fmt.Println("node started...")
+	time.Sleep(hotspotTime)
+}
+
+// Routine automation routine
+func Routine() {
+	startUp()
+	for true {
+		nodeLoop()
+		time.Sleep(nodeResetTime)
+	}
+}
