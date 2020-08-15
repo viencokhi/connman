0efb4f63d96d359dd54111458e2d214afd5b7bc0
emre <ocakemrecan@gmail.com>
2020-08-16 00:24:58 +0300

    sudo prefix added to cmd

--- spot_core.go
+++ spot_core.go
@@ -7,26 +7,26 @@ import (
 
 func addSpot(name, pass string) error {
 	var cmd string
-	cmd = fmt.Sprintf(`nmcli con add type wifi ifname "%v" con-name "%v" autoconnect yes ssid "%v"`, wifiInterface, name, name)
+	cmd = fmt.Sprintf(`sudo nmcli con add type wifi ifname "%v" con-name "%v" autoconnect yes ssid "%v"`, wifiInterface, name, name)
 	out, err := exe(cmd, "add spot")
 	if err != nil {
 		return fmt.Errorf("error:%v, out:%v", err.Error(), out)
 	}
 
-	cmd = fmt.Sprintf(`nmcli con modify "%v" 802-11-wireless.mode ap 802-11-wireless.band bg ipv4.method shared`, name)
+	cmd = fmt.Sprintf(`sudo nmcli con modify "%v" 802-11-wireless.mode ap 802-11-wireless.band bg ipv4.method shared`, name)
 	out, err = exe(cmd, "modify conn spot")
 	if err != nil {
 		return fmt.Errorf("error:%v, out:%v", err.Error(), out)
 	}
 
 	if pass != "" {
-		cmd = fmt.Sprintf(`nmcli con modify "%v" wifi-sec.key-mgmt wpa-psk`, name)
+		cmd = fmt.Sprintf(`sudo nmcli con modify "%v" wifi-sec.key-mgmt wpa-psk`, name)
 		out, err = exe(cmd, "modify pass type spot")
 		if err != nil {
 			return fmt.Errorf("error:%v, out:%v", err.Error(), out)
 		}
 
-		cmd = fmt.Sprintf(`nmcli con modify "%v" wifi-sec.psk "%v"`, name, pass)
+		cmd = fmt.Sprintf(`sudo nmcli con modify "%v" wifi-sec.psk "%v"`, name, pass)
 		out, err = exe(cmd, "modify pass spot")
 		if err != nil {
 			return fmt.Errorf("error:%v, out:%v", err.Error(), out)
@@ -37,7 +37,7 @@ func addSpot(name, pass string) error {
 
 // up, down, delete
 func setSpot(mode, name string) error {
-	cmd := fmt.Sprintf(`nmcli con "%v" "%v"`, mode, name)
+	cmd := fmt.Sprintf(`sudo nmcli con "%v" "%v"`, mode, name)
 	out, err := exe(cmd, fmt.Sprintf(`modify "%v" spot`, mode))
 	if err != nil {
 		return fmt.Errorf("error:%v, out:%v", err.Error(), out)
@@ -47,7 +47,7 @@ func setSpot(mode, name string) error {
 
 //SpotExists spot exists return bool
 func spotExists(name string) (bool, error) {
-	cmd := fmt.Sprintf(`nmcli -f NAME con sh`)
+	cmd := fmt.Sprintf(`sudo nmcli -f NAME con sh`)
 	out, err := exe(cmd, "spot scan")
 	if err != nil {
 		return false, fmt.Errorf("error:%v, out:%v", err.Error(), out)
